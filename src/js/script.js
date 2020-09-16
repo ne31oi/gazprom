d7d76df35b840427553d0e1319a69bb3da7bae83
ik <ik@callibri.ru>
2020-09-16 12:05:32 +0500

    +

--- src/js/script.js
+++ src/js/script.js
@@ -49,7 +49,7 @@ var scene = new ScrollMagic.Scene({ triggerElement: "header", duration: $("heade
         if (mesh) {
             mesh.rotation.x = -0.15 * (e.progress - proc);
             var head_text = document.querySelector(".head_text");
-            head_text.style.zIndex = (e.progress > 0.55) ? 2 : 3;
+            head_text.style.zIndex = (e.progress > 0.75) ? 2 : 3;
             $(".head_text h1").css({ "transform": "scale(" + (1 - (e.progress - proc)) + ") translateY(-" + 300 * (e.progress - proc) + "px)", "opacity": 1 - (e.progress - proc) });
             $(".head_text .subtext").css({ "transform": "scale(" + (1 - (e.progress - proc)) + ") translateY(-" + 370 * (e.progress - proc) + "px)", "opacity": 1 - (e.progress - proc) });
         }
@@ -68,25 +68,83 @@ var scene = new ScrollMagic.Scene({ triggerElement: ".years", duration: $(".year
         $(".data-item").each(function() {
             var n = Math.round(parseInt($(this).find("span").attr("data-num"))),
                 no = parseInt($(this).find("span").attr("data-num-old")),
-                diff = no-n;
+                diff = no - n;
             $(this).find("span").text(Math.round(n + e.progress.toFixed(1) * diff) + "%");
-            /*if ($(this).hasClass("data-item1") || $(this).hasClass("data-item2") || $(this).hasClass("data-item3")) {
-                if (n > no) {
-                    $(this).find("span").text(n + "%");
-                } else {
-                    $(this).find("span").text(no + "%");
-                }
-            } else {
-                if (n < no) {
-                    $(this).find("span").text(n + "%");
-                } else {
-                    $(this).find("span").text(no + "%");
-                }
-            }
-            console.log(n)*/
-            //$(this).text(n > parseInt($(this).attr("data-num-old"))) ? (parseInt($(this).attr("data-num-old"): n) + "%");
+
         });
         $(".year").text(2018 + Math.round(e.progress.toFixed(1) * 22));
+    });
+var scene = new ScrollMagic.Scene({ triggerElement: "#img4_3", duration: $("#img4_3").outerHeight() })
+    .setTween(tween)
+    .addTo(controller)
+    .on("progress", function(e) {
+        $("#img4_3").css({ "width": 100 * e.progress + "%" });
+        if (e.progress < 0.44) {
+            $("#img4_1").css({ "width": 0 });
+        } else if (e.progress < 0.51) {
+            $("#img4_1").css({ "width": '48%' });
+        } else if (e.progress < 0.57) {
+            $("#img4_1").css({ "width": "55%" });
+        } else if (e.progress < 0.64) {
+            $("#img4_1").css({ "width": "60%" });
+        } else { $("#img4_1").css({ "width": "80%" }); }
+    });
+var scene = new ScrollMagic.Scene({ triggerElement: "#img7_1", duration: $("#img7_1").outerHeight() / 1.5 })
+    .setTween(tween)
+    .addTo(controller)
+    .on("progress", function(e) {
+        $("#img7_3").css({ "width": 100 * e.progress + "%" });
+        $("#img7_2").css({ "width": 100 * e.progress + "%" });
+        if (e.progress < 0.02) {
+            $("#img7_4").css({ "width": 0 });
+            $("#img7_5").css({ "width": 0 });
+            $("#img7_6").css({ "width": 0 });
+            $("#img7_7").css({ "width": 0 });
+        } else if (e.progress < 0.15) {
+            $("#img7_4").css({ "width": '5%' });
+            $("#img7_5").css({ "width": '27%' });
+            $("#img7_6").css({ "width": '27%' });
+            $("#img7_7").css({ "width": 0 });
+        } else if (e.progress < 0.27) {
+            $("#img7_4").css({ "width": "17%" });
+            $("#img7_5").css({ "width": '27%' });
+            $("#img7_6").css({ "width": '27%' });
+            $("#img7_7").css({ "width": 0 });
+        } else if (e.progress < 0.40) {
+            $("#img7_4").css({ "width": "33%" });
+            $("#img7_5").css({ "width": '40%' });
+            $("#img7_6").css({ "width": '27%' });
+            $("#img7_7").css({ "width": 0 });
+        } else if (e.progress < 0.53) {
+            $("#img7_4").css({ "width": "44%" });
+            $("#img7_5").css({ "width": '80%' });
+            $("#img7_6").css({ "width": '100%' });
+            $("#img7_7").css({ "width": 0 });
+        } else if (e.progress < 0.65) {
+            $("#img7_4").css({ "width": "56%" });
+            $("#img7_5").css({ "width": '80%' });
+            $("#img7_6").css({ "width": '100%' });
+            $("#img7_7").css({ "width": 0 });
+        } else if (e.progress < 0.80) {
+            $("#img7_4").css({ "width": "74%" });
+            $("#img7_5").css({ "width": '80%' });
+            $("#img7_6").css({ "width": '100%' });
+            $("#img7_7").css({ "width": '100%' });
+        } else {
+            $("#img7_4").css({ "width": "100%" });
+            $("#img7_5").css({ "width": '100%' });
+            $("#img7_6").css({ "width": '100%' });
+            $("#img7_7").css({ "width": '100%' });
+        }
+    });
+var scene = new ScrollMagic.Scene({ triggerElement: ".answ1 .fade", duration: $(".answ1 .fade").outerHeight() + 300 })
+    .setTween(tween)
+    .addTo(controller)
+    .on("progress", function(e) {
+        var top = $(".answ1 .fade").outerHeight() * e.progress;
+        $(".answ1").css({ "margin-bottom": "-" + ($(".answ1 .fade").outerHeight() - top) + "px" });
+
+        $(".section04").css({ "opacity": e.progress });
 
     });
 var drawGrid = function(w, h, id) {
@@ -130,9 +188,9 @@ var isMouseDown = false;
 
 function init() {
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
     camera.position.z = 53.84;
-    camera.position.y = 2;
+    camera.position.y = 3;
     camera.position.x = -2;
     var canva = document.getElementById("main-canvas");
     renderer = new THREE.WebGLRenderer({ alpha: true });
@@ -144,23 +202,23 @@ function init() {
     var light1 = new THREE.DirectionalLight("#72f4ff", 1);
     var light2 = new THREE.DirectionalLight("#72f4ff", 1);
     var light3 = new THREE.DirectionalLight("#72f4ff", 1);
-    light.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xff0040 })));
+   /* light.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xff0040 })));
     light1.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xf500ff })));
     light2.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x0a00ff })));
-    light3.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x00fff5 })));
+    light3.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x00fff5 })));*/
     var ambient = new THREE.AmbientLight("#85b2cd");
 
-    ambient.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x0000ff })));
+    /*ambient.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x0000ff })));
     //light.color.setHSL( 0, 0, 1 );
     light.position.set(-30, 30, 25);
     light1.position.set(30, 30, 25);
     light2.position.set(-30, -30, 25);
-    light3.position.set(30, -30, 25);
+    light3.position.set(30, -30, 25);*/
 
 
     var materials = [];
 
-    scene.fog = new THREE.Fog(0x01012f, 10, 100);
+    scene.fog = new THREE.Fog(0x01012f, -20, 60);
 
     var manager = new THREE.LoadingManager();
     manager.onProgress = function(item, loaded, total) {};
@@ -184,7 +242,7 @@ function init() {
             materials.push(new THREE.MeshBasicMaterial({ color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending }));
             materials.push(new THREE.MeshBasicMaterial({ color: 0x3c88b8, transparent: true }));
 
-            materials.push(new THREE.MeshLambertMaterial({ color: 0xdddddd }));
+         /*   materials.push(new THREE.MeshLambertMaterial({ color: 0xdddddd }));
             materials.push(new THREE.MeshPhongMaterial({ color: 0xdddddd, specular: 0x009900, shininess: 30, map: texture, transparent: true }));
             materials.push(new THREE.MeshNormalMaterial({ flatShading: true }));
 
@@ -193,16 +251,18 @@ function init() {
             materials.push(new THREE.MeshDepthMaterial());
             materials.push(new THREE.MeshLambertMaterial({ color: 0x666666, emissive: 0xff0000 }));
             materials.push(new THREE.MeshPhongMaterial({ color: 0x000000, specular: 0x666666, emissive: 0xff0000, shininess: 10, opacity: 0.9, transparent: true }));
-            materials.push(new THREE.MeshBasicMaterial({ map: texture, transparent: true }));
+            materials.push(new THREE.MeshBasicMaterial({ map: texture, transparent: true }));*/
 
 
             mesh = gltf.scene;
             mesh.scale.set(40, 40, 40);
             mesh.rotation.y = -0.2;
+            console.log(mesh)
             mesh.traverse((o) => {
                 if (o.isMesh) o.material = materials[5];
             });
             scene.add(mesh);
+
 
         },
         function(xhr) {},
@@ -219,18 +279,28 @@ function init() {
         if ($(this).hasClass("item4")) {
             $(".part__answer.answer1.false").hide();
             $(".part__answer.answer1.true").css('display', 'flex');
+            $("#quiz1 .part__quiz__item").removeClass('false');
+            $(this).addClass('true');
         } else {
             $(".part__answer.answer1.false").css('display', 'flex');
             $(".part__answer.answer1.true").hide();
+            $("#quiz1 .part__quiz__item").removeClass('false');
+            $("#quiz1 .part__quiz__item").removeClass('true');
+            $(this).addClass('false');
         }
     });
     $("#quiz2 .part__quiz__item").on("click", function(e) {
-        if ($(this).hasClass("item2")) {
+        if ($(this).hasClass("item3")) {
             $(".part__answer.answer2.false").hide();
             $(".part__answer.answer2.true").css('display', 'flex');
+            $("#quiz2 .part__quiz__item").removeClass('false');
+            $(this).addClass('true');
         } else {
             $(".part__answer.answer2.false").css('display', 'flex');
             $(".part__answer.answer2.true").hide();
+            $("#quiz2 .part__quiz__item").removeClass('false');
+            $("#quiz2 .part__quiz__item").removeClass('true');
+            $(this).addClass('false');
         }
     });
 }
